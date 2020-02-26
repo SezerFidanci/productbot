9244c3f8d43cadc786930edbf537ddb30b687540
Sezer Fidanci <sezerfidanci@gmail.com>
2020-02-26 16:41:23 +0300

    release

--- example.php
+++ example.php
@@ -12,7 +12,7 @@ require_once __DIR__ . '/autoload.php';
     //$data = $product->crawPage("https://www.trendyol.com/trendyolmilla/siyah-yuksek-bel-basic-orme-tayt-twoaw20ta0047-p-30560409");
     //$data = $product->crawPage("https://www.adidas.com.tr/tr/3-bantli-tisort/CW1202.html");
     //$data = $product->crawPage("https://www.decathlon.com.tr/mh100-polar-id_8492976.html");
-    //$data = $product->crawPage("https://urun.n11.com/sac-kurutma-ve-fon-makinesi/powertec-tr-901-profesyonel-fon-makinesi-ve-sac-kurutma-makinesi-P256180546");
+
     $data = $product->crawPage("https://www.pullandbear.com/tr/erkek/ayakkab%C4%B1/spor-ayakkab%C4%B1lar/mikro-delikli-spor-ayakkab%C4%B1-c1030207036p501858058.html?cS=010");
     echo json_encode($data) ;
 
