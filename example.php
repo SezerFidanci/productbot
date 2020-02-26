562e86cea8511eb8f7a8bb3d7e79c68eb65ca3df
Sezer Fidanci <sezerfidanci@gmail.com>
2020-02-26 10:52:01 +0300

    second

--- example.php
+++ example.php
@@ -4,4 +4,4 @@ require_once __DIR__ . '/autoload.php';
 use ProductBot as PB;
 
 
-echo json_encode(PB::crawPage("awd"));
+json_encode(PB::crawPage("awd"));
--- src/ProductBot.php
+++ src/ProductBot.php
@@ -1,5 +1,5 @@
 <?php
-
+namespace ProductBot;
 
 class ProductBot
 {
